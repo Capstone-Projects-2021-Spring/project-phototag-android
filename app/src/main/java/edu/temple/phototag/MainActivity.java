85d5fd9ff8c1f2f1b6670c6ed47faddfeda1536c
James Coolen <tug52370@temple.edu>
2021-04-03 14:10:04 -0400

    Created ScheduleFragment w/ UI. Clicking the Schedule Tags button in Settings Fragment will now load this fragment.

--- app/src/main/java/edu/temple/phototag/MainActivity.java
+++ app/src/main/java/edu/temple/phototag/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements SettingsFragment.
                 != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST);
         } else {
-            Toast.makeText(this, "Permission denied to local photos", Toast.LENGTH_LONG).show();
+            //Toast.makeText(this, "Permission denied to local photos", Toast.LENGTH_LONG).show();
             //callback
         }
         fm = getSupportFragmentManager();
@@ -479,6 +479,11 @@ public class MainActivity extends AppCompatActivity implements SettingsFragment.
 
     @Override
     public void loadScheudleFragment() {
+        //begin fragment
+        fm.beginTransaction()
+                .replace(R.id.main, ScheduleFragment.newInstance())
+                .addToBackStack(null)
+                .commit();
         Log.d("SCHEDULE", "Schedule fragment loaded.");
     }
 
