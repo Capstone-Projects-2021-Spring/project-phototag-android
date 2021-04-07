afb7802188cc8defb6d9d1f8485b8fbd0d85a906
James Coolen <tug52370@temple.edu>
2021-04-07 16:34:16 -0400

    UI changes. Date and Time picker being added

--- app/src/main/java/edu/temple/phototag/ScheduleFragment.java
+++ app/src/main/java/edu/temple/phototag/ScheduleFragment.java
@@ -7,9 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
 
 public class ScheduleFragment extends Fragment {
 
+    //UI variables
+    Button next1;
+    Button next2;
+    EditText scheduleName_Input;
+    TextView scheduleName_Header;
+    TextView startDate_Header;
+    DatePicker datePicker1;
 
     public ScheduleFragment() {
         // Required empty public constructor
@@ -31,8 +46,60 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        View v =  inflater.inflate(R.layout.fragment_schedule, container, false);
+
+        //Set up UI variables.
+        scheduleName_Header = v.findViewById(R.id.scheduleName_Header);
+        scheduleName_Input = v.findViewById(R.id.scheduleName_Input);
+        datePicker1 = v.findViewById(R.id.datePicker1);
+        startDate_Header = v.findViewById(R.id.startDate_Header);
+        //Next button that shows under Schedule Name.
+        next1 = v.findViewById(R.id.next1);
+        next1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               hideScheduleNameDisplay();
+               showStartDateDisplay();
+            }
+        });
+
+        //Next button that shows under start date.
+        next2 = v.findViewById(R.id.next2);
+        next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideStartDateDisplay();
+                showEndDateDisplay();
+            }
+        });
+
+        return v;
     }
+
+    // ***************** UI METHODS BEGIN *****************
+    private void hideScheduleNameDisplay() {
+        scheduleName_Header.setVisibility(View.GONE);
+        scheduleName_Input.setVisibility(View.GONE);
+        next1.setVisibility(View.GONE);
+    }
+
+    private void hideStartDateDisplay() {
+        datePicker1.setVisibility(View.GONE);
+        startDate_Header.setVisibility(View.GONE);
+        next2.setVisibility(View.GONE);
+    }
+
+    private void showStartDateDisplay() {
+        datePicker1.setVisibility(View.VISIBLE);
+        startDate_Header.setVisibility(View.VISIBLE);
+        next2.setVisibility(View.VISIBLE);
+    }
+
+    private void showEndDateDisplay() {
+
+    }
+
+    // ***************** UI METHODS END *****************
 
 
 }//end class
